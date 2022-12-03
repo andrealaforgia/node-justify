2d3aa3c8882807a29fad42ce871af48f30152568
tariba <tariqxa@gmail.com>
2022-12-03 15:45:01 +0000

    Plan to solve the problem

--- justification.js
+++ justification.js
@@ -1,3 +1,22 @@
+//Create a function that will justify the column 
+//when a string is passed along with maxcol the funtion has to justify the column
+//string will always be euqal or < than maxCol
+
+//Plan:
+//Will check the length of the word if the word length is euqal to maxCol will return that str
+// Else will check the length of each word and compare it to the column
+//will fit the words in each column depending on the length of the word
+//if only one word can be fitted in the column then will have space after the word to meet the column requirement 
+//if two words or more words can be fit in a column will fit the words and have even spaces between them to meet the column requirement
+//in order to have even spaces between two or more workds will count the words and subtract that from the maxCol and will divide the spaces evenly
+
+
+
+
+
+
+
+
 
 function justify(str, maxCol) {
     let justifiedLines = []
